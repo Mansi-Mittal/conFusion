1412df80ac878aa6908d418fdca2308fa59bdfa8
Mansi Mittal <m16mittal@gmail.com>
2017-10-28 00:19:01 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,MatListModule,MatGridListModule, MatCardModule, MatButtonModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule,MatListModule,MatGridListModule, MatCardModule } from '@angular/material'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component'
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
+
+import {AppRoutingModule} from './app-routing/app-routing.module'
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule,BrowserAnimationsModule,MatToolbarModule,FlexLayoutModule,MatListModule,MatGridListModule,MatCardModule
+    BrowserModule,BrowserAnimationsModule,MatToolbarModule,FlexLayoutModule,MatListModule,MatGridListModule,MatCardModule,MatButtonModule,AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService,
+         PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,8 +1,13 @@
-export class Dish{
+import { Comment } from './comment';
+
+export class Dish {
+    id:number;
     name: string;
     image: string;
     category: string;
     label: string;
     price: string;
+    featured : boolean;
     description: string;
+    comments: Comment[];
 }
